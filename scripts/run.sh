bb7f329533ade54d1b398f21f44f0efe0ee754ff
Mir Mazedur Rahaman <mazedm80@gmail.com>
2023-10-30 19:02:07 +0000

    run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,13 @@
-export API_DEBUG=True
+#!/bin/bash
+
+export API__PORT=8000
+export API__DEBUG=True
+export API__SECRET_KEY=""
+
+export PSQL__HOST="mirserver"
+export PSQL__PORT=5432
+export PSQL__USER="htmx"
+export PSQL__PASSWORD="htmx123"
+export PSQL__DATABASE="pyhtmx"
 
 python3 main.py
